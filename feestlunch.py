a585bc8ca857e68fc360bc40bcf9ef5a73d03b92
Lucas van Pelt <lucasvpelt03@gmail.com>
2021-09-09 11:35:51 +0200

    F1.2.03.L2

---
+++ feestlunch.py
@@ -0,0 +1,13 @@
+Croissant = 17
+CroissantPrijs = 0.39
+Stokbrood = 2
+StokbroodPrijs = 2.78
+Kortingsbon = 3
+KortingsbonPrijs = 0.50
+Bedrag = 10.69
+
+Centen = int(Bedrag * 100)
+
+Uitkomst = "De feestlunch kost je bij de bakker " + str(Centen) + " cent " + "voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!"
+
+print(Uitkomst) 
---
+++ speelhaldag.py
@@ -0,0 +1,12 @@
+Toegangsticket = 7.45
+Personen = 3
+GameSeatMinuten = 5
+GameSeatTotaal = 45
+GameSeatPrijs = 0.37
+Bedrag = 25.68
+
+Centen = int(Bedrag * 100)
+
+Uitkomst = "Dit geweldige dagje-uit met 3 mensen in de Speelhal met 45 minuten VR kost je maar " + str(Centen) + " cent"
+
+print(Uitkomst)
